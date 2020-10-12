308f36933a030c32ac493a1052dd329f056fc972
olina <kontorerer@gmail.com>
2020-10-12 17:43:58 +0300

    Updates

---
+++ precache-manifest.1f917c9ae5b5784aec2a025ca025261f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bd56bd3a2958c0ea8c83cc58725b42",
+    "url": "/react-pie-chart/index.html"
+  },
+  {
+    "revision": "94cdc2be3f01bdf8f48b",
+    "url": "/react-pie-chart/static/css/main.f6d87acb.chunk.css"
+  },
+  {
+    "revision": "9a65e941a17834bc3501",
+    "url": "/react-pie-chart/static/js/2.d7fcb5ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pie-chart/static/js/2.d7fcb5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cdc2be3f01bdf8f48b",
+    "url": "/react-pie-chart/static/js/main.f8db2439.chunk.js"
+  },
+  {
+    "revision": "1a6e0a1a59acb3121718",
+    "url": "/react-pie-chart/static/js/runtime-main.5c66f911.js"
+  }
+]);
